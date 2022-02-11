fa30c191d808238bfad3162639e321da8010efdc
Jan Janssen <jan.janssen@outlook.com>
2022-02-11 16:15:27 -0700

    Implement DatabaseInterface

--- pygmailfilter/message_tables.py
+++ pygmailfilter/message_tables.py
@@ -1,69 +1,134 @@
-import pandas
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-def build_thread_table(df):
-    db_id_lst, email_id_lst, thread_id_lst = [], [], []
-    for i, [eid, tid] in enumerate(zip(df["id"], df["thread_id"])):
-        db_id_lst.append(i)
-        email_id_lst.append(eid)
-        thread_id_lst.append(tid)
-    return pandas.DataFrame(
-        {"id": db_id_lst, "email_id": email_id_lst, "thread_id": thread_id_lst}
-    )
+Base = declarative_base()
 
 
-def build_label_table(df):
-    db_id_lst, email_id_lst, label_id_lst = [], [], []
-    i = 0
-    for eid, lid_lst in zip(df["id"], df["label_ids"]):
-        for label in lid_lst:
-            db_id_lst.append(i)
-            email_id_lst.append(eid)
-            label_id_lst.append(label)
-            i += 1
-    return pandas.DataFrame(
-        {"id": db_id_lst, "email_id": email_id_lst, "label_id": label_id_lst}
-    )
+class Threads(Base):
+    __tablename__ = "threads"
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String)
+    thread_id = Column(String)
 
 
-def get_email_address(email):
-    email_split = email.split("<")
-    if len(email_split) == 1:
-        return email.lower()
-    else:
-        return email_split[1].split(">")[0].lower()
+class Labels(Base):
+    __tablename__ = "labels"
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String)
+    label_id = Column(String)
 
 
-def build_email_index(df, colum_to_index):
-    db_id_lst, email_id_lst, column_id_lst = [], [], []
-    i = 0
-    for eid, email_lst in zip(df["id"], df[colum_to_index]):
-        email_split_lst = email_lst.split(", ")
-        for email_add in [get_email_address(email=email) for email in email_split_lst]:
-            db_id_lst.append(i)
-            email_id_lst.append(eid)
-            column_id_lst.append(email_add)
-            i += 1
-    return db_id_lst, email_id_lst, column_id_lst
+class Email_to(Base):
+    __tablename__ = "email_to"
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String)
+    email_to = Column(String)
 
 
-def build_email_to_table(df):
-    db_id_lst, email_id_lst, recipe_id_lst = build_email_index(
-        df=df, colum_to_index="to"
-    )
-    return pandas.DataFrame(
-        {"id": db_id_lst, "email_id": email_id_lst, "email_to": recipe_id_lst}
-    )
+class Email_from(Base):
+    __tablename__ = "email_from"
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String)
+    email_from = Column(String)
 
 
-def build_email_from_table(df):
-    db_id_lst, email_id_lst, recipe_id_lst = build_email_index(
-        df=df, colum_to_index="from"
-    )
-    return pandas.DataFrame(
-        {"id": db_id_lst, "email_id": email_id_lst, "email_from": recipe_id_lst}
-    )
+class Email_content(Base):
+    __tablename__ = "email_content"
+    id = Column(Integer, primary_key=True)
+    email_id = Column(String)
+    email_subject = Column(String)
+    email_content = Column(String)
 
 
-def build_content_table(df):
-    return df.drop(["thread_id", "label_ids", "to", "from"], axis=1)
+def create_database_session(engine):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    return session
+
+
+class DatabaseInterface:
+    def __init__(self, engine):
+        self._session = self.create_database_session(engine=engine)
+
+    def commit_thread_table(self, df):
+        self._session.add_all(
+            [
+                Threads(email_id=email_id, thread_id=thread_id)
+                for email_id, thread_id in zip(df["id"], df["thread_id"])
+            ]
+        )
+        self._session.commit()
+
+    def commit_label_table(self, df):
+        label_lst = []
+        for email_id, lid_lst in zip(df["id"], df["label_ids"]):
+            for label_id in lid_lst:
+                label_lst.append(Labels(email_id=email_id, label_id=label_id))
+        self._session.add_all(label_lst)
+        self._session.commit()
+
+    def commit_email_to_table(self, df):
+        email_id_lst, recipe_id_lst = self.build_email_index(df=df, colum_to_index="to")
+        self._session.add_all(
+            [
+                Email_to(email_id=email_id, email_to=email_to)
+                for email_id, email_to in zip(email_id_lst, recipe_id_lst)
+            ]
+        )
+        self._session.commit()
+
+    def commit_email_from_table(self, df):
+        email_id_lst, recipe_id_lst = self.build_email_index(
+            df=df, colum_to_index="from"
+        )
+        self._session.add_all(
+            [
+                Email_from(email_id=email_id, email_from=email_from)
+                for email_id, email_from in zip(email_id_lst, recipe_id_lst)
+            ]
+        )
+        self._session.commit()
+
+    def commit_content_table(self, df):
+        df_content = df.drop(["thread_id", "label_ids", "to", "from"], axis=1)
+        self._session.add_all(
+            [
+                Email_content(
+                    email_id=email_id,
+                    email_subject=email_subject,
+                    email_content=email_content,
+                )
+                for email_id, email_subject, email_content in zip(
+                    df_content["id"], df_content["subject"], df_content["content"]
+                )
+            ]
+        )
+        self._session.commit()
+
+    @staticmethod
+    def create_database_session(engine):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        Base.metadata.create_all(engine)
+        return session
+
+    @staticmethod
+    def get_email_address(email):
+        email_split = email.split("<")
+        if len(email_split) == 1:
+            return email.lower()
+        else:
+            return email_split[1].split(">")[0].lower()
+
+    def build_email_index(self, df, colum_to_index):
+        email_id_lst, column_id_lst = [], []
+        for eid, email_lst in zip(df["id"], df[colum_to_index]):
+            email_split_lst = email_lst.split(", ")
+            for email_add in [
+                self.get_email_address(email=email) for email in email_split_lst
+            ]:
+                email_id_lst.append(eid)
+                column_id_lst.append(email_add)
+        return email_id_lst, column_id_lst
--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
         'google-auth==2.6.0',
         'google-auth-oauthlib==0.4.6',
         'tqdm==4.62.3',
-        'pandas==1.4.0'
+        'pandas==1.4.0',
+        'sqlalchemy==1.4.31'
     ],
     cmdclass=versioneer.get_cmdclass(),
     entry_points={
