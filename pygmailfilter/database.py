552ba1c8a1ef5b318b279221d69fee00b8dc8523
Jan Janssen <jan.janssen@outlook.com>
2022-02-11 16:40:32 -0700

    database interface: hide internal functions

--- pygmailfilter/database.py
+++ pygmailfilter/database.py
@@ -19,21 +19,21 @@ class Labels(Base):
     label_id = Column(String)
 
 
-class Email_to(Base):
+class EmailTo(Base):
     __tablename__ = "email_to"
     id = Column(Integer, primary_key=True)
     email_id = Column(String)
     email_to = Column(String)
 
 
-class Email_from(Base):
+class EmailFrom(Base):
     __tablename__ = "email_from"
     id = Column(Integer, primary_key=True)
     email_id = Column(String)
     email_from = Column(String)
 
 
-class Email_content(Base):
+class EmailContent(Base):
     __tablename__ = "email_content"
     id = Column(Integer, primary_key=True)
     email_id = Column(String)
@@ -41,16 +41,9 @@ class Email_content(Base):
     email_content = Column(String)
 
 
-def create_database_session(engine):
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base.metadata.create_all(engine)
-    return session
-
-
 class DatabaseInterface:
     def __init__(self, engine):
-        self._session = self.create_database_session(engine=engine)
+        self._session = self._create_database_session(engine=engine)
 
     def commit_thread_table(self, df):
         self._session.add_all(
@@ -70,22 +63,22 @@ class DatabaseInterface:
         self._session.commit()
 
     def commit_email_to_table(self, df):
-        email_id_lst, recipe_id_lst = self.build_email_index(df=df, colum_to_index="to")
+        email_id_lst, recipe_id_lst = self._build_email_index(df=df, colum_to_index="to")
         self._session.add_all(
             [
-                Email_to(email_id=email_id, email_to=email_to)
+                EmailTo(email_id=email_id, email_to=email_to)
                 for email_id, email_to in zip(email_id_lst, recipe_id_lst)
             ]
         )
         self._session.commit()
 
     def commit_email_from_table(self, df):
-        email_id_lst, recipe_id_lst = self.build_email_index(
+        email_id_lst, recipe_id_lst = self._build_email_index(
             df=df, colum_to_index="from"
         )
         self._session.add_all(
             [
-                Email_from(email_id=email_id, email_from=email_from)
+                EmailFrom(email_id=email_id, email_from=email_from)
                 for email_id, email_from in zip(email_id_lst, recipe_id_lst)
             ]
         )
@@ -95,7 +88,7 @@ class DatabaseInterface:
         df_content = df.drop(["thread_id", "label_ids", "to", "from"], axis=1)
         self._session.add_all(
             [
-                Email_content(
+                EmailContent(
                     email_id=email_id,
                     email_subject=email_subject,
                     email_content=email_content,
@@ -107,28 +100,27 @@ class DatabaseInterface:
         )
         self._session.commit()
 
+    def _build_email_index(self, df, colum_to_index):
+        email_id_lst, column_id_lst = [], []
+        for eid, email_lst in zip(df["id"], df[colum_to_index]):
+            email_split_lst = email_lst.split(", ")
+            for email_add in [
+                self._get_email_address(email=email) for email in email_split_lst
+            ]:
+                email_id_lst.append(eid)
+                column_id_lst.append(email_add)
+        return email_id_lst, column_id_lst
+
     @staticmethod
-    def create_database_session(engine):
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    def _create_database_session(engine):
+        session = sessionmaker(bind=engine)()
         Base.metadata.create_all(engine)
         return session
 
     @staticmethod
-    def get_email_address(email):
+    def _get_email_address(email):
         email_split = email.split("<")
         if len(email_split) == 1:
             return email.lower()
         else:
             return email_split[1].split(">")[0].lower()
-
-    def build_email_index(self, df, colum_to_index):
-        email_id_lst, column_id_lst = [], []
-        for eid, email_lst in zip(df["id"], df[colum_to_index]):
-            email_split_lst = email_lst.split(", ")
-            for email_add in [
-                self.get_email_address(email=email) for email in email_split_lst
-            ]:
-                email_id_lst.append(eid)
-                column_id_lst.append(email_add)
-        return email_id_lst, column_id_lst
